1b42dc96f50a9ca06654797bad7031e271a67106
Drago <kratosmusasa@gmail.com>
2026-01-13 11:43:59 +0200

    horror

--- models/expense.js
+++ models/expense.js
@@ -4,6 +4,7 @@ const ExpenseSchema = new mongoose.Schema({
   businessId: { type: mongoose.Schema.Types.ObjectId, ref: "Business", required: true },
   branchId: { type: mongoose.Schema.Types.ObjectId, ref: "Branch" },
   amount: { type: Number, required: true },
+  description: { type: String },
   category: { type: String },
   method: { type: String },
   createdBy: { type: String }
